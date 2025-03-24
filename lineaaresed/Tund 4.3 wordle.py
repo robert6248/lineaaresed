8a49274b78c7cb7f5d122818af0ba41feb431f96
David-Lennuk <david.nikolajev07@gmail.com>
2025-03-24 11:38:45 +0200

    tund 4.3 wordle

---
+++ lineaaresed/Tund 4.3 wordle.py
@@ -0,0 +1,22 @@
+from random import *
+sõnad=["cat","dog","cow","mouse","hen","lizard","ant","bee","wasp","fly","rat","fish","horse","goat","pig"]
+rnd=choice(wrds)
+rndlist=list(rnd)
+k=len(rndlist)
+for i in range(0,6,1):
+    o-=1
+    print("Guess the word. You have ", 6-i,"attempts")
+    answer=input("[] "*k)
+    answerlist=list(answer)
+    for a in range(0,k,1):
+        o+=1
+        if answerlist[o]==rndlist[o]:
+            print(answerlist[o],end=" ")
+        else:
+            print("*",end=" ")
+    print()
+    if answer==rnd:
+        print("Correct! the word was ", rnd, )
+        break
+    else:
+        print("Wrong! ", 6-i-1, "attemps remains")
