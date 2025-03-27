337ce4e2cd3098964ac23ea8d0aa17be84538d51
David-Lennuk <david.nikolajev07@gmail.com>
2025-03-27 10:59:43 +0200

    kasutaja funktsioonide 1-7 ülesanned

--- lineaaresed/funktsioonide kasutamine.py
+++ lineaaresed/funktsioonide kasutamine.py
@@ -1,20 +1,72 @@
-from kasutaja_funktsioonide import *
+﻿from kasutaja_funktsioonide import *
+from math import *
+# a=float(input("Arv1:"))
+# b=float(input("Arv2:"))
+# t=input("Tehe:")
+# vastus=arithmetic(a,b,t)
+# print(vastus)
 
-a=float(input("Arv1:"))
-b=float(input("Arv2:"))
-t=input("Tehe:")
-vastus=arithmetic(a,b,t)
-print(vastus)
+# vastus=arithmetic(float(input("Arv1:")),float(input("Arv2:")),input("Tehe:"))
+# print(vastus)
 
-vastus=arithmetic(float(input("Arv1:")),float(input("Arv2:")),input("Tehe:"))
-print(vastus)
+# #is_year_leap funktsiooni kasutamine
+# aasta=int(input("Mis aasta tahad kontrolliida?? "))
+# if aasta>0:
+#     v=is_year_leap(aasta)
+#     print(v)
+#     if v:
+#         print(f"{aasta} on liigaasta")
+#     else:
+#         print(f"{aasta} ei ole liigasta")
 
-#is_year_leap funktsiooni kasutamine
-aasta=int(input("Mis aasta tahad kontrolliida?? "))
-if aasta>:
-    v=is_year_leap(aasta)
-    print(v)
-    if v:
-        print(f"{aasta} on liigaasta")
-    else:
-        print(f"{aasta} ei ole liigasta")
+#square() kasutamine:
+#kontroll while True ja try except
+# while True:
+#     try:
+#         a = float(input("Ruudu külg= "))
+#         if a <= 0:
+#             print("Сторона квадрата должна быть положительным числом. Попробуйте снова.")
+#             continue
+#         perimeter, pindala, diagonal = square(a)
+#         print(f"Ruudu perimeeter: {perimeter}")
+#         print(f"Ruudu pindala: {area}")
+#         print(f"Ruudu diagonaal: {diagonal}")
+#         break
+#     except:
+#         print("Viga! Palun sisestage arvu väärtus ruudu külje jaoks.")
+
+#season
+# while True:
+#     try:
+#         month = int(input("Sisesta kuu number (1-12): "))
+#         if 1 <= month <= 12:
+#             print(f"See kuu kuulub: {season(month)}") 
+#         else:
+#             print("Palun sisesta kuu number vahemikus 1-12.")  
+#     except:
+#         print("Viga! Palun sisesta number.") 
+
+
+# #bank
+# while True:
+#     try:
+#         summa = float(input("Sisesta algne summa (eurodes): "))
+#         aastad = int(input("Sisesta aastate arv: "))
+#         if summa <= 0 or aastad <= 0:
+#             print("Summa ja aastad peavad olema positiivsed arvud. Proovige uuesti.")
+#         final_sum = bank(summa, aastad)
+#         print(f"Summa {aastad} aasta pärast: {final_sum} eurot.")
+#     except:
+#         print("Viga! Palun sisestage korrektne number.")
+
+#numbrid
+# while True:
+#     try:
+#         number = int(input("Sisesta number, et kontrollida, kas see on algarv (0-1000): "
+#         if 0 <= number <= 1000:
+#             print(f"Kas {number} on algarv? {is_prime(number)}")
+#         else:
+#             print("Palun sisesta number vahemikus 0 kuni 1000.")
+#     except ValueError:
+#         print("Viga! Palun sisestage korrektne number.")
+# random_number = randint(0, 1000)
--- lineaaresed/kasutaja_funktsioonide.py
+++ lineaaresed/kasutaja_funktsioonide.py
@@ -1,32 +1,106 @@
-def arithmetic(arv1:float,arv2:float,tehe:str)-> any:
-    """Funktsioon töötab nagu lihtne kalkuaator
-    + - liitmine
-    - - lahutamine
-    * - korrutamine
-    / - jagamine
-    Kui sisend ei ole järjendus[+,-,/,*],siis tagastab sõne "Tundmatu tehe"
-    :param float arv1: Sisend ujukomaarvi tüübina
-    :param float arv2: Sisend ujukomaarvi tüübina
-    :param str tehe: Sisend listist [+,-,/,*]
-    :rtype: varMääramata tüüp (float või str)
-    """
-    if tehe  in ["+","-","*","/"]:
-        if arv2==0 and tehe=="/":
-            vastus="DIV/0"
-        else:
-            vastus=eval(str(arv1)+tehe+str(arv2))
-    else:
-        vastus="Tundmatu teha"
-    return vastus
+#1
+# def arithmetic(arv1:float,arv2:float,tehe:str)-> any:
+#     """Funktsioon töötab nagu lihtne kalkuaator
+#     + - liitmine
+#     - - lahutamine
+#     * - korrutamine
+#     / - jagamine
+#     Kui sisend ei ole järjendus[+,-,/,*],siis tagastab sõne "Tundmatu tehe"
+#     :param float arv1: Sisend ujukomaarvi tüübina
+#     :param float arv2: Sisend ujukomaarvi tüübina
+#     :param str tehe: Sisend listist [+,-,/,*]
+#     :rtype: varMääramata tüüp (float või str)
+#     """
+#     if tehe  in ["+","-","*","/"]:
+#         if arv2==0 and tehe=="/":
+#             vastus="DIV/0"
+#         else:
+#             vastus=eval(str(arv1)+tehe+str(arv2))
+#     else:
+#         vastus="Tundmatu teha"
+#     return vastus
 
-def is_year_leap(aasta:int)->bool:
-    """Liigaasta leidmine
-    Tagastab True, kui aasta on liigaasta ja False kui aasta on tavaline aasta
-    :param int aasta:Sisend kasutajalt
-    :rtype bool tõeväärsuses formaadis tulemuus
+#2
+# def is_year_leap(aasta:int)->bool:
+#     """Liigaasta leidmine
+#     Tagastab True, kui aasta on liigaasta ja False kui aasta on tavaline aasta
+#     :param int aasta:Sisend kasutajalt
+#     :rtype bool tõeväärsuses formaadis tulemuus
+#     """
+#     if aasta%4==0:
+#         v=True
+#     else:
+#         v=False
+#         return v
+
+#3
+# def square(külg:float)->any:
+#     """
+#     Funktsioon tagastab ruudu ümbermõõt, ruudu pindala ja ruuudu diagonaal
+#     """
+#     S=külg**2
+#     P=4*külg
+#     d=(2)**(1/2)*külg
+#     return S,P,d
+
+#4
+# def season(param: int) -> str:
+#     """
+#     Funktsioon, mis võtab vastu kuu numbri ja tagastab vastava aastaaja nimetuse.
+#     :param param: Kuu number (1-12)
+#     :rtype: String, mis sisaldab aastaaja nimetust (talv, kevad, suvi, sügis)
+#     """
+#     if param in [1, 2, 12]:
+#         H = "Talv"  
+#     elif param in [3, 4, 5]:
+#         H = "Kevad"  
+#     elif param in [6, 7, 8]:
+#         H = "Suvi"  
+#     elif param in [9, 10, 11]:
+#         H = "Sügis"  
+#     else:
+#         H = "Vale kuu" 
+#     return H
+
+#5
+# def bank(summa: float, aastad: int)->float:
+#     """
+#     Funktsioon arvutab kasutaja pangakontol oleva summa koos intressidega.
+#     :param summa: algne sissemakse summa
+#     :param aastad: aastate arv
+#     :return: lõppsumma pärast kõiki intresse
+#     """
+#     for aasta in range(aastad):
+#         summa*=1.1
+#     return summa
+
+from random import *
+# #6
+# def is_prime(a=randint(1,10000))->bool:
+#     """
+#     Funktsioon, mis kontrollib, kas number on algarv.
+#     :param a: Arv, mille kohta kontrollitakse, kas see on algarv (0 kuni 1000)
+#     :return: True, kui number on algarv, vastasel juhul False
+#     """
+#     print(a)
+#     v=True
+#     for jagaja in range(2,a):
+#         if a%jagaja==0:
+#             v=False
+#     return v
+
+#7
+def date(päev:int,kuu:int,aasta:int)->bool:
     """
-    if aasta%4==0:
+    """
+    if päev in range(1,32) and kuu in [1,3,5,7,8,10,12] and aasta>0:
+        v=True
+    elif päev in range(1,30) and kuu==2 and is_year_leap(aasta):
+        v=True
+    elif päev in range(1,29) and kuu==2 and not is_year_leap(aasta):
+        v=True
+    elif päev in range(1,31) and kuu in[4,6,9,11] and aasta>0:
         v=True
     else:
         v=False
-        return v
+    return v
